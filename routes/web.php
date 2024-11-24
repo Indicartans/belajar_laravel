bb5950c036ac36e1a71fa8d630c84e7435293ef0
indicartans <indica464@gmail.com>
2024-11-24 23:31:17 +0700

    xendit payment gateway

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/product/{id}', [HomeController::class, 'detail'])->name('detail-product');
+Route::post('/payment', [HomeController::class, 'payment'])->name('payment');
+Route::get('/notification/{id}', [HomeController::class, 'notification'])->name('notification');
